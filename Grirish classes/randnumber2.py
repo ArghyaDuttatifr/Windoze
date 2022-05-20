fef88ba2a91adc139bd7a31627691f293ac03142
Arghya Dutta <arghyaduttabku@gmail.com>
2022-05-20 13:58:42 +0530

    last class

---
+++ Grirish classes/randnumber2.py
@@ -0,0 +1,32 @@
+from cmath import pi
+from tkinter import Y
+import numpy as np
+import matplotlib.pyplot as plt
+'''np.pi = pi
+n = 2000
+x_range = np.linspace(0, 5, 1000)
+y_func = 2*np.sqrt(2/pi) * np.exp(-x_range**2 /2)
+x = np.random.random(n)*10
+y = np.random.random(n)*1
+print (x)
+x_good = []
+for i in range(n):
+    if (y[i]<np.sqrt(2/pi) * np.exp(-x[i]**2 /2) ):
+        x_good.append(x[i])
+plt.hist(x_good, bins= 100)
+plt.scatter(y, x, vmin=-0.0, vmax=1.1, lw=0, s=5)
+plt.plot(x_range, y_func)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+plt.show()
